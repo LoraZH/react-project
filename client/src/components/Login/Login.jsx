3a2b9e75c3dc5556da31ddfa9408c6f2355bc0d3
LoraZH <lora_zh@abv.bg>
2024-07-16 08:21:03 +0300

    change login

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,37 +1,50 @@
-import { useState } from 'react';
-import './Login.css'
+import useForm from '../../hooks/useForm';
+import { Link } from 'react-router-dom';
+import './Login.css';
 
 export default function Login() {
+  const submitHandler = (values) => {
+    console.log('Form submitted with values:', values);
+  };
+
+  const { values, onChange, onSubmit } = useForm({ email: '', psw: '' }, submitHandler);
+
   return (
-    <div style={{width: "100%"}}>
-    <form action="action_page.php" method="post" >
-      <div className="p-4">
-        <label htmlFor="email">
-          <b>Email</b>
-        </label>
-        <input type="text" placeholder="john_d@abv.bg" name="email" required="" />
-        <label htmlFor="psw">
-          <b>Password</b>
-        </label>
-        <input
-          type="password"
-          placeholder="*********"
-          name="psw"
-          required=""
-        />
-        <button type="submit" >Login</button>
-        <div className="container signup">
-    <p>
-            Don't have an account? <a href="/register">Sign up</a>.
-    </p>
-  </div>
-       
-
-      </div>
-      <div className="p-4" style={{ backgroundColor: "#f1f1f1" }}>
-      </div>
-    </form>
+    <div style={{ width: "100%" }}>
+      <form action="action_page.php" method="post" onSubmit={onSubmit}>
+        <div className="p-4">
+          <label htmlFor="email">
+            <b>Email</b>
+          </label>
+          <input
+            type="text"
+            placeholder="john_d@abv.bg"
+            id="email"
+            name="email"
+            required=""
+            onChange={onChange}
+            value={values.email}
+          />
+          <label htmlFor="psw">
+            <b>Password</b>
+          </label>
+          <input
+            type="password"
+            placeholder="*********"
+            name="psw"
+            required=""
+            onChange={onChange}
+            value={values.psw}
+          />
+          <button type="submit">Login</button>
+          <div className="container signup">
+            <div>
+              Don't have an account? <Link to="/register">Sign up</Link>.
+            </div>
+          </div>
+        </div>
+        <div className="p-4" style={{ backgroundColor: "#f1f1f1" }}></div>
+      </form>
     </div>
-
-  )
+  );
 }
