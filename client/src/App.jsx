3e6f662f9fb19cb4524ec0495983ca4566a8aa15
LoraZH <lora_zh@abv.bg>
2024-07-31 11:01:11 +0300

    add usePersistedState

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -24,51 +24,9 @@ import Logout from './components/Logout/Logout'
 
 
 export default function App() {
-  const navigate = useNavigate();
-  const [auth, setAuth] = useState(() => {
-    localStorage.removeItem('accessToken');
-    return {};
-  });
-
-  const loginSubmitHandler = async (values) => {
-    try {
-      const result = await authService.login(values.email, values.password);
-      setAuth(result);
-      localStorage.setItem('accessToken', result.accessToken);
-      navigate(Path.Home);
-    } catch (error) {
-      console.error('Login error:', error);
-       }
-  };
-
-  const registerSubmitHandler = async (values) => {
-    try {
-      const result = await authService.register(values.email, values.password);
-      setAuth(result);
-      localStorage.setItem('accessToken', result.accessToken);
-      navigate(Path.Home);
-    } catch (error) {
-      console.error('Register error:', error);
-     }
-  }
-
-  const logoutHandler = () => {
-    setAuth({});
-    localStorage.removeItem('accessToken');
-  }
-
-  const values = {
-    loginSubmitHandler,
-    registerSubmitHandler,
-    logoutHandler,
-    username: auth.username || auth.email,
-    email: auth.email,
-    isAuthenticated: !!auth.email,
-  }
-
-
+  
   return (
-    <AuthProvider value={values}>
+    <AuthProvider>
       <>
         <Header />
         <Navigation />
