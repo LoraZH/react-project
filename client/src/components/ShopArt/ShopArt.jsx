ab3dd1804118b87ee055f551f151c9da29cd90d4
LoraZH <lora_zh@abv.bg>
2024-08-01 10:36:00 +0300

    update shop

--- client/src/components/ShopArt/ShopArt.jsx
+++ client/src/components/ShopArt/ShopArt.jsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react"
-import * as ArtServices from '../../services/ArtServices'
+import { useEffect, useState } from "react";
+import * as ArtServices from '../../services/ArtServices';
 import ShopArtItem from "./ShopArtItem/ShopArtItem";
 import Categories from "../Categories/Categories";
 
@@ -10,29 +10,28 @@ export default function Shop() {
     ArtServices.getAll()
       .then(result => setArt(result))
   }, []);
-  console.log(art);
+
   return (
     <>
-      <div className=" p-4 py-5">
+      <div className="p-4 py-5">
         <div className="row">
           <Categories />
           <div className="col-lg-9">
             <div className="row">
-              {art.map(art => (
-                <ShopArtItem key={art._id}{...art} />
+              {art.map(artItem => (
+                <div className="col-md-4 mb-4" key={artItem._id}>
+                  <ShopArtItem {...artItem} />
+                </div>
               ))}
             </div>
-            <div div="row">
+            <div className="row">
               <ul className="pagination pagination-lg justify-content-end">
+                {/* Pagination items */}
               </ul>
             </div>
           </div>
         </div>
       </div>
-   
-      
-      
-
     </>
-  )
+  );
 }
--- client/src/components/ShopArt/ShopArtItem/ShopArtItem.jsx
+++ client/src/components/ShopArt/ShopArtItem/ShopArtItem.jsx
@@ -1,5 +1,6 @@
 import '../ShopArtItem/ShopArtItem.css';
 import { Link } from 'react-router-dom';
+
 export default function ShopArtItem({
   _id,
   title,
@@ -7,70 +8,55 @@ export default function ShopArtItem({
   imageUrl,
 }) {
   return (
-    <div className="container py-5">
-      <div className="row">
-
-        <div className="col-lg-9">
-          <div className="row">
-            <div className="col-md-4">
-              <div className="card mb-4 product-wap rounded-0">
-                <div className="card rounded-0">
-                  <img
-                    className="card-img rounded-0 img-fluid"
-                    src={imageUrl}
-                  />
-                  <div className="card-img-overlay rounded-0 product-overlay d-flex align-items-center justify-content-center">
-                    <ul className="list-unstyled">
-                      <li>
-                        <a
-                          className="btn btn-success text-white"
-                          href="/"
-                        >
-                          <i className="far fa-heart" />
-                        </a>
-                      </li>
-                      <li>
-                      <Link 
-                          className="btn btn-success text-white mt-2"
-                          to={`/art/${_id}`}
-                        >
-                          <i className="far fa-eye" />
-                        </Link>
-                        
-                      </li>
-                      <li>
-                        <Link
-                          className="btn btn-success text-white mt-2"
-                          to={`/cart`}
-                        >
-                          <i className="fas fa-cart-plus" />
-                        </Link>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-                <div className="card-body">
-                <Link to={`/art/${_id}`} className="h3 text-decoration-none"> {title} </Link>
-                  <ul className="list-unstyled d-flex justify-content-center mb-1">
-                    <li>
-                      <i className="text-warning fa fa-star" />
-                      <i className="text-warning fa fa-star" />
-                      <i className="text-warning fa fa-star" />
-                      <i className="text-muted fa fa-star" />
-                      <i className="text-muted fa fa-star" />
-                    </li>
-                  </ul>
-                  <p className="text-center mb-0">{`$${price}`}</p>
-                </div>
-              </div>
-            </div>
-  
-          </div>
-
+    <div className="card mb-4 product-wap rounded-0">
+      <div className="card rounded-0">
+        <img
+          className="card-img rounded-0 img-fluid"
+          src={imageUrl}
+          alt={title}
+        />
+        <div className="card-img-overlay rounded-0 product-overlay d-flex align-items-center justify-content-center">
+          <ul className="list-unstyled">
+            <li>
+              <a
+                className="btn btn-success text-white"
+                href="/"
+              >
+                <i className="far fa-heart" />
+              </a>
+            </li>
+            <li>
+              <Link 
+                className="btn btn-success text-white mt-2"
+                to={`/art/${_id}`}
+              >
+                <i className="far fa-eye" />
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="btn btn-success text-white mt-2"
+                to={`/cart`}
+              >
+                <i className="fas fa-cart-plus" />
+              </Link>
+            </li>
+          </ul>
         </div>
       </div>
+      <div className="card-body">
+        <Link to={`/art/${_id}`} className="h3 text-decoration-none"> {title} </Link>
+        <ul className="list-unstyled d-flex justify-content-center mb-1">
+          <li>
+            <i className="text-warning fa fa-star" />
+            <i className="text-warning fa fa-star" />
+            <i className="text-warning fa fa-star" />
+            <i className="text-muted fa fa-star" />
+            <i className="text-muted fa fa-star" />
+          </li>
+        </ul>
+        <p className="text-center mb-0">{`$${price}`}</p>
+      </div>
     </div>
-
-
-  )
+  );
 }
